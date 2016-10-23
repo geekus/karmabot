89fb0076e83ce53eed7a4d66ff1504100dfc0b1b
Roar Skaar <roar.skaar@gmail.com>
2016-10-23 13:12:03 +0200

    fix(karma): add separate template strings and unit testing

--- scripts/karma.js
+++ scripts/karma.js
@@ -1,42 +1,50 @@
+// Description:
+//   Give all the karma
+
+
+const tpl = require('../lib/response-template-strings');
+
+
 module.exports = function hubot(robot) {
   const incrementRegExp = /@([a-z0-9_\-\.]+)\s?\+{2,}/ig
   const decrementRegExp = /@([a-z0-9_\-\.]+)\s?\-{2,}/ig
   const adjustRegExp = /@([a-z0-9_\-\.]+)\s?[\+\-]{2,}/ig;
 
   robot.hear(adjustRegExp, (res) => {
-    const {robot, message, match, envelope} = res;
-    // const sendingUser = msg.message.user.name;
-    let botResponse = '';
-    const increments = message.text.match(incrementRegExp) || [];
-    const decrements = message.text.match(decrementRegExp) || [];
+    const message = res.message;
+    const botResponse = [];
+    const increments = message.text.match(incrementRegExp) || [];
+    const decrements = message.text.match(decrementRegExp) || [];
 
     increments.forEach((match) => {
       const user = match.replace(/^@([\w\.\-]+)\s?\+{2,}$/, '$1');
-      robot.brain.set('test', 20);
 
       if (user !== message.user.name) {
-        const count = (robot.brain.get(user) || 0) + 1;
+        const count = (robot.brain.get(user) || 0) + 1;
         robot.brain.set(user, count);
-        botResponse = `${botResponse}Woot! @${user} now at ${count} — Gz!\n`;
+        botResponse.push(tpl.increment.format({ user, count }));
       } else {
-        botResponse = `${botResponse}Nice try @${user}! :wink:`;
+        botResponse.push(tpl.karmaToSelf.format({ user }));
       }
     });
 
     decrements.forEach((match) => {
       const user = match.replace(/^@([\w\.\-]+)\s?\-{2,}$/, '$1');
-      const count = (robot.brain.get(user) || 0);
+      let count = robot.brain.get(user) || 0;
 
       if (count > 0) {
-        count--;
+        count -= 1;
         robot.brain.set(user, count);
-        botResponse = `${botResponse}OMG! @${user} now at ${count} — Sorry!\n`;
+        botResponse.push(tpl.decrement.format({ user, count }));
       } else {
-        botResponse = `${botResponse}@${user} is already at 0 — Can't get much lower. :grimacing:\n`;
+        botResponse.push(tpl.decrementError.format({ user }));
       }
-
     });
 
-    return res.send(`${botResponse}`);
+    if (botResponse.length) {
+      return res.send(botResponse.join('\r\n'));
+    }
+
+    return null;
   });
 };
--- test/unit/karma-test.js
+++ test/unit/karma-test.js
@@ -1,17 +1,80 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const co = require('co');
-const assert = require('assert');
-const {expect} = require('chai');
-
+const { expect } = require('chai');
+const tpl = require('../../lib/response-template-strings');
 const Helper = require('hubot-test-helper');
+
 const helper = new Helper('../scripts');
-let room = undefined;
+let room;
 
 describe('Karmabot', () => {
   beforeEach(() => {
     room = helper.createRoom();
   });
 
+
+  it('tells the karma after a single user received positive karma', (done) => {
+    co(() => {
+      room.user.say('alice', '@goodguy ++');
+    }).then(() => {
+      const res = tpl.increment.format({ user: 'goodguy', count: 1 });
+      expect(room.messages[1][1]).to.be.eq(res);
+      done();
+    }).catch(done);
+  });
+
+
+  it('tells the karma after a single user received negative karma', (done) => {
+    co(() => {
+      room.robot.brain.set('goodguy', 1);
+      room.user.say('alice', '@goodguy --');
+    }).then(() => {
+      const res = tpl.decrement.format({ user: 'goodguy', count: 0 });
+      expect(room.messages[1][1]).to.be.eq(res);
+      done();
+    }).catch(done);
+  });
+
+
+  it('tells the karma is already at 0 after a single user received negative karma', (done) => {
+    co(() => {
+      room.user.say('alice', '@goodguy --');
+    }).then(() => {
+      const res = tpl.decrementError.format({ user: 'goodguy' });
+      expect(room.messages[1][1]).to.be.eq(res);
+      done();
+    }).catch(done);
+  });
+
+
+  it('tells the karma after several users received karma', (done) => {
+    co(() => {
+      room.user.say('alice', '@goodguy ++ @goodguy++ @badguy --');
+    }).then(() => {
+      const res = [];
+      res.push(tpl.increment.format({ user: 'goodguy', count: 1 }));
+      res.push(tpl.increment.format({ user: 'goodguy', count: 2 }));
+      res.push(tpl.decrementError.format({ user: 'badguy', count: 0 }));
+
+      expect(room.messages[1][1]).to.be.eq(res.join('\r\n'));
+      done();
+    }).catch(done);
+  });
+
+
+  it('does not allow a user to increment itself', (done) => {
+    co(() => {
+      room.robot.brain.set('alice', 1);
+      room.user.say('alice', '@alice ++');
+    }).then(() => {
+      expect(room.messages[1][1]).to.be.eq(tpl.karmaToSelf.format({ user: 'alice' }));
+      expect(room.robot.brain.get('alice')).to.be.eq(1);
+      done();
+    }).catch(done);
+  })
+
+
   afterEach(() => {
-   room.destroy();
+    room.destroy();
   });
 });
